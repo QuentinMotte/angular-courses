bbbc12f87a174a750f69caa3b02f56cfaab4d06c
Quentin <motte.quentin1@gmail.com>
2024-03-26 14:36:17 +0100

    Robot-shop start Angular fundamentals
    Add SiteHeader and Catalog components to the app
    
    Imported the SiteHeader and Catalog components into the main component in AppComponent.ts script. Adjusted the imports array in the AppComponent decorator to include both SiteHeaderComponent and CatalogComponent. Also, cleaned up the app.component.html and removed the placeholder content.

---
+++ robot-shop/src/app/app.component.ts
@@ -0,0 +1,15 @@
+import {Component} from '@angular/core';
+import {RouterOutlet} from '@angular/router';
+import {SiteHeaderComponent} from "./site-header/site-header.component";
+import {CatalogComponent} from "./catalog/catalog.component";
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, SiteHeaderComponent, CatalogComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'robot-shop';
+}
---
+++ robot-shop/src/app/site-header/site-header.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'bot-site-header',
+  templateUrl: './site-header.component.html',
+  styleUrls: ['./site-header.component.css'],
+  standalone: true
+})
+export class SiteHeaderComponent {
+
+}
